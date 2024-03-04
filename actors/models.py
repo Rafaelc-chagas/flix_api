4412e911233b9cd875bce8984cc7db309ffc6f1e
Rafael Chagas <rafaelcchagas12@gmail.com>
2024-03-04 18:24:53 -0300

    my first commit

---
+++ actors/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+NATINALITY_CHOICES = (
+    ('USA', 'Estados Unidos'),
+    ('BRA', 'Brasil'),
+)
+
+class Actor(models.Model):
+    name = models.CharField(max_length=200)
+    birthday = models.DateField(null=True, blank=True)
+    nationality = models.CharField(
+        max_length=100,
+        choices=NATINALITY_CHOICES,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+    
